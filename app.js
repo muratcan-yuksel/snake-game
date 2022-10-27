d0e487bc4916fa588a3fe5393f910e812cf9a536
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-10-28 02:39:37 +0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+class SnakePart {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 let speed = 7;
 let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
@@ -8,6 +15,9 @@ let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 //y position of the head of the snake
 let headY = 10;
+
+const snakeParts = [];
+let tailLength = 2;
 //to control the position of the snake via keyboard
 let xVelocity = 0;
 let yVelocity = 0;
@@ -15,15 +25,79 @@ let yVelocity = 0;
 let appleX = 5;
 let appleY = 5;
 
+let score = 0;
+
 //game loop
 function drawGame() {
-  clearScreen();
   changeSnakePosition();
+  let result = isGameOver();
+  if (result) {
+    return;
+  }
+
+  clearScreen();
   changeAppleCollision();
   drawApple();
   drawSnake();
-  console.log("drawGame");
+  drawScore();
+  //   console.log("drawGame");
   setTimeout(drawGame, 1000 / speed);
+}
+
+function isGameOver() {
+  let gameOver = false;
+
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  //walls
+  if (headX < 0) {
+    gameOver = true;
+  } else if (headX === tileCount) {
+    gameOver = true;
+  } else if (headY < 0) {
+    gameOver = true;
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
+    }
+  }
+
+  if (gameOver) {
+    ctx.fillStyle = "white";
+    ctx.font = "50px Verdana";
+
+    if (gameOver) {
+      ctx.fillStyle = "white";
+      ctx.font = "50px Verdana";
+
+      var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+      gradient.addColorStop("0", " magenta");
+      gradient.addColorStop("0.5", "blue");
+      gradient.addColorStop("1.0", "red");
+      // Fill with gradient
+      ctx.fillStyle = gradient;
+
+      ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    }
+
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+  }
+
+  return gameOver;
+}
+
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "10px Verdana";
+  ctx.fillText("Score " + score, canvas.width - 50, 10);
 }
 
 function clearScreen() {
@@ -34,6 +108,16 @@ function clearScreen() {
 function drawSnake() {
   ctx.fillStyle = "orange";
   ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+  snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift(); // remove the furthet item from the snake parts if have more than our tail size.
+  }
 }
 
 function changeSnakePosition() {
@@ -52,6 +136,10 @@ function changeAppleCollision() {
     //generate a new apple on a random location
     appleX = Math.floor(Math.random() * tileCount);
     appleY = Math.floor(Math.random() * tileCount);
+    //increase the tail length
+    tailLength++;
+    //increase the score
+    score++;
   }
 }
 
